f61fe96d6f29b422ac1ec1edc468cabf1487b4d8
Salim Ramirez <salimramirezm@gmail.com>
2026-03-01 16:39:36 -0500

    chore: primer commit

---
+++ src/main/java/com/liquilabs/vankoo/discovery/VankooDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.liquilabs.vankoo.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VankooDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VankooDiscoveryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liquilabs/vankoo/discovery/VankooDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquilabs.vankoo.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VankooDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
