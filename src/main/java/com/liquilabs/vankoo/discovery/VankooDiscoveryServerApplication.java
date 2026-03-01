3e14c61d7aa09310603bf505601ba4f7a6a6f061
Salim Ramirez <salimramirezm@gmail.com>
2026-03-01 17:04:45 -0500

    feat: agregar archivos de configuración para diferentes entornos y habilitar el servidor de Eureka

--- src/main/java/com/liquilabs/vankoo/discovery/VankooDiscoveryServerApplication.java
+++ src/main/java/com/liquilabs/vankoo/discovery/VankooDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.liquilabs.vankoo.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class VankooDiscoveryServerApplication {
 
     public static void main(String[] args) {
